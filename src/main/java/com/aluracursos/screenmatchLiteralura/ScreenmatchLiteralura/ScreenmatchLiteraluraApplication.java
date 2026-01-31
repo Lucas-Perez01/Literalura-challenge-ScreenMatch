ff92a9905db8c0ffd4d8324dd9706cd1e91f8adb
Lucas <paragithub01@gmail.com>
2026-01-31 02:58:48 -0300

    first commit

---
+++ src/main/java/com/aluracursos/screenmatchLiteralura/ScreenmatchLiteralura/ScreenmatchLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatchLiteralura.ScreenmatchLiteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchLiteraluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/screenmatchLiteralura/ScreenmatchLiteralura/ScreenmatchLiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatchLiteralura.ScreenmatchLiteralura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchLiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
